838da2a472e9cd9dbda01bc8c29ea7b4f30aa535
AndiCucka <124384763+AndiCucka@users.noreply.github.com>
2024-04-12 09:28:23 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -8,34 +8,16 @@
 
 import { createPrompt } from 'bun-promptx';
 
-const prompt = createPrompt();
-
 function reverseString(str: string): string {
-    return str.split('').reverse().join('');
-}
-
-function writeUpDown(str: string): string {
-    return str.split('').join('\n');
+  return str.split('').reverse().join('');
 }
 
 async function main() {
-    const word = await prompt('Enter a word: ');
-
-    const option = await prompt({
-        message: 'Choose an option:',
-        choices: ['Reverse the word', 'Write the word from top to bottom']
-    });
-
-    switch (option) {
-        case 'Reverse the word':
-            console.log("Reversed word:", reverseString(word));
-            break;
-        case 'Write the word from top to bottom':
-            console.log("Word written from top to bottom:\n", writeUpDown(word));
-            break;
-        default:
-            console.log("Invalid option!");
-    }
+  const prompt = createPrompt();
+  const userInput = await prompt('Enter a word to reverse: ');
+  const reversedWord = reverseString(userInput);
+  console.log(`Reversed word: ${reversedWord}`);
+  prompt.close();
 }
 
-main().catch(err => console.error(err));
+main().catch(console.error);
